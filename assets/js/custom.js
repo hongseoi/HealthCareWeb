64ebcbe43d319e39ed60b999caadc65d1d317e86
seoi hong <90823683+hongseoi@users.noreply.github.com>
2022-06-14 21:28:58 +0900

    최종

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,26 +21,71 @@ $('button').on('click', function () {
     $('#delay').val(hours + ':' + minutes);
 });
 
-var calories = document.getElementById("calories").value;
+var totalscore = 0;
 var sleeptime = document.getElementById("delay").value;
-var works = document.getElementById("works").value;
 
 function foodscore(){
-    if(calories>=2000) document.write("칼로리 섭취량을 줄이세요");
-    else if(works>=1500) document.write("칼로리 섭취량 적절함");
-    else document.write("No data");
+    var calories = eval(document.getElementById("calories").value);
+    if (calories>2000){
+        document.getElementById("calreturn").value = "일일 섭취량을 줄이세요";
+    }
+    else if(calories>=1500){
+        totalscore += 1;
+        document.getElementById("calreturn").value = "칼로리 섭취량이 적절합니다.";
+    }
+    else{
+        document.getElementById("calreturn").value = "더 많이 먹으세요";
+    }
+
 }
 
 function sleepscore(){
-    if(hours>9) document.write("수면시간을 줄이세요.");
-    else if(hours>=7) document.write("수면 시간이 적절합니다.");
-    else document.write("수면시간이 부족합니다.");
+
+    var sleeptime = eval(document.getElementById("delay").value);
+    
+    if (sleeptime>9){
+        document.getElementById("sleepreturn").value = "수면 시간을 줄이세요";
+    }
+    else if(sleeptime>6){
+        totalscore += 1;
+        document.getElementById("sleepreturn").value = "수면시간이 적절합니다.";
+    }
+    else{
+        document.getElementById("sleepreturn").value = "좀 더 주무세요";
+    }
 
 }
 
 function exerscore(){
-    if(works>=7000) document.write("A");
-    else if(works>=5000) document.write("B");
-    else if(works>=3000) document.write("C");
-    else document.write("No data");
+
+    var exercise = eval(document.getElementById("exercise").value);
+
+    if (exercise>9){
+        document.getElementById("exreturn").value = "운동을 줄이세요";
+    }
+    else if(exercise>=1500){
+        totalscore += 1;
+        document.getElementById("exreturn").value = "적절합니다.";
+    }
+    else{
+        document.getElementById("exreturn").value = "좀 더 걸으세요";
+    }
+
+
+}
+
+function totallscore (){
+
+    this.totalscore = totalscore;
+
+    if (totalscore==3){
+        document.getElementById("totalreturn").value = "A";
+    }
+    else if(totalscore==2){
+        document.getElementById("totalreturn").value = "B";
+
+    }
+    else{
+        document.getElementById("totalreturn").value = "C";
+    }
 }
